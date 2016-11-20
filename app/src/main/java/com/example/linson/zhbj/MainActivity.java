baca7580952890cca378cf4bc56c981de8250213
song linson <song linson>
2016-11-20 11:12:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/linson/zhbj/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.linson.zhbj;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //123
+    }
+}
