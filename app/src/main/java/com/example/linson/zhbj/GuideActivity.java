679b807635cff147b68df9e922c63fe21c0382c9
linson0116 <linson0116@163.com>
2016-11-23 16:25:54 +0800

    1.1 20161123

--- app/src/main/java/com/example/linson/zhbj/GuideActivity.java
+++ app/src/main/java/com/example/linson/zhbj/GuideActivity.java
@@ -4,13 +4,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 public class GuideActivity extends Activity {
 
@@ -18,6 +22,7 @@ public class GuideActivity extends Activity {
     private List<ImageView> mList;
     private LinearLayout ll_points;
     private int[] mPics;
+    private View v_point_red;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,20 +50,25 @@ public class GuideActivity extends Activity {
         mPics = new int[]{R.mipmap.guide_1, R.mipmap.guide_2, R.mipmap.guide_3};
         vp_guide = (ViewPager) findViewById(R.id.vp_guide);
         ll_points = (LinearLayout) findViewById(R.id.ll_points);
+        v_point_red = findViewById(R.id.v_point_red);
         for (int i = 0; i < mPics.length; i++) {
-            View view = new View(this);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(50, 50);
+            ImageView view = new ImageView(this);
+            view.setBackgroundResource(R.drawable.points_gray);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             if (i != 0) {
-                params.leftMargin = 50;
+                params.leftMargin = 10;
             }
             view.setLayoutParams(params);
-            view.setBackgroundResource(R.drawable.points_gray);
-            ll_points.addView(view, i);
+            ll_points.addView(view);
         }
         vp_guide.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
+                Log.i(TAG, "onPageScrolled: position=" + position + " positionOffset=" + positionOffset + " positionOffsetPixels=" + positionOffsetPixels);
+                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) v_point_red.getLayoutParams();
+                layoutParams.leftMargin = (int) (40 * (position + positionOffset));
+                v_point_red.setLayoutParams(layoutParams);
             }
 
             @Override
